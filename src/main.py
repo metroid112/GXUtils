04919c6fe1d8072c331832bca2e2da6a6da90fe5
metroid112 <federico.aguilera112@gmail.com>
2019-06-24 15:25:52 -0300

    kivy

--- src/main.py
+++ src/main.py
@@ -1,25 +1,23 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from functools import partial
 
 
-class App(QWidget):
+class MainWindow(App):
+    def disable(self, instance, *args):
+        instance.disabled = True
 
-    def __init__(self):
-        super().__init__()
-        self.title = 'PyQt5 simple window - pythonspot.com'
-        self.left = 10
-        self.top = 10
-        self.width = 640
-        self.height = 480
-        self.init_ui()
+    def update(self, instance, *args):
+        instance.text = "I am Disabled!"
 
-    def init_ui(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-        self.show()
+    def build(self):
+        button = Button(text="Click me to disable", pos=(300, 350), size_hint=(.25, .18))
+        button.bind(on_press=partial(self.disable, button))
+        button.bind(on_press=partial(self.update, button))
+        return button
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = App()
-    sys.exit(app.exec_())
+    MainWindow().run()
