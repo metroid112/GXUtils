f30b50c61156dc82f2913097f27a60823fef6a24
metroid112 <federico.aguilera112@gmail.com>
2019-06-24 16:39:24 -0300

    xml

--- src/cmd.py
+++ src/cmd.py
@@ -24,6 +24,7 @@ def choose_table():
 
 
 def main():
+    core.descartes(['form'])
     print_start()
     command = choose_command()
     # For Each
--- src/core.py
+++ src/core.py
@@ -1,5 +1,16 @@
+import xml.etree.cElementTree as ElementTree
+
+
 def command_new(table):
     print(f'New\t\\\\{table}')
     for x in range(10):
         print('\tAttr = &Attr')
     print('EndNew')
+
+
+def descartes(elements):
+    xml = ElementTree.Element('object-definition', libraryName="Dlya.Basic", libraryVersion="1.0", designer="WebUI", designerDate="06/02/2013")
+    for element in elements:
+        if element == 'form':
+            ElementTree.SubElement(xml, 'form')
+    xml_tree = ElementTree.ElementTree(xml).write('xml.xml')
